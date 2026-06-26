cdfb5e37e080073d4cdfd2e859407a2882929cad
Steven Tran <sdtran91@gmail.com>
2026-06-26 15:45:22 -0700

    add tests for task model and validations

--- task-manager-api/spec/models/task_spec.rb
+++ task-manager-api/spec/models/task_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.create(first_name: "Naruto", last_name: "Uzumaki", email: "hokage@gmail.com", password: "believeit!")}
+
+  it "is valid with required info" do 
+    task = Task.new(title: "Clean living room", status: "in_progress", user: user)
+    expect(task).to be_valid
+  end
+
+  it "is invalid without title" do 
+    task = Task.new(title: nil, description: "I will become hokage of the leaf village", status: "in_progress", user: user)
+    expect(task).not_to be_valid
+  end
+
+  it "is invalid with nil status" do 
+    task = Task.new(title: "Go grocery shopping", description: "Buy ingredients for ramen", status: nil, user: user)
+    expect(task).not_to be_valid
+  end
+
+  it "is invalid with invalid status" do 
+    task = Task.new(title: "Clean house", status: "do_tomorrow", user: user)
+    expect(task).not_to be_valid
+  end
 end
