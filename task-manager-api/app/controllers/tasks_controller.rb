7786f1c31c33f807f7df44110cd5378c5b3a81e3
Steven Tran <sdtran91@gmail.com>
2026-06-26 11:25:51 -0700

    add create action and task params

--- task-manager-api/app/controllers/tasks_controller.rb
+++ task-manager-api/app/controllers/tasks_controller.rb
@@ -10,6 +10,13 @@ class TasksController < ApplicationController
   end
 
   def create
+    task = @current_user.tasks.new(task_params)
+    if task.save
+      Rails.cache.delete("tasks:user_#{@current_user.id}")
+      render json: task, status: :created 
+    else
+      render json: task.errors, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -17,4 +24,10 @@ class TasksController < ApplicationController
 
   def destroy
   end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description, :status, :due_date)
+  end
+
 end
