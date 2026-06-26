7e3a1bf406795bb20827736fda71433805f3bd38
Steven Tran <sdtran91@gmail.com>
2026-06-26 13:02:51 -0700

    built full CRUD for task controller

--- task-manager-api/app/controllers/tasks_controller.rb
+++ task-manager-api/app/controllers/tasks_controller.rb
@@ -7,6 +7,11 @@ class TasksController < ApplicationController
   end
 
   def show
+    task = @current_user.tasks.find(params[:id])
+    render json: task
+  rescue ActiveRecord::RecordNotFound
+    render json: {error: 'Task not found'}, status: :not_found
+
   end
 
   def create
@@ -20,9 +25,24 @@ class TasksController < ApplicationController
   end
 
   def update
+    task = @current_user.tasks.find(params[:id])
+    if task.update(task_params)
+      Rails.cache.delete("tasks:user_#{@current_user.id}")
+      render json: task, status: :ok
+    else
+      render json: task.errors, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+      render json: {error: 'Task not found'}, status: :not_found
   end
 
   def destroy
+    task = @current_user.tasks.find(params[:id])
+    task.destroy
+    Rails.cache.delete("tasks:user_#{@current_user.id}")
+    head :no_content
+  rescue ActiveRecord::RecordNotFound
+    render json: {error: 'Task not found'}, status: :not_found
   end
 
   private
